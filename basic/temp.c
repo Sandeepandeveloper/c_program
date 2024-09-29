ec31df31ae5376223a01ff3ebdc8f0a229081213
Sandeepandeveloper <sandeepan.n.behera@gmail.com>
2024-09-29 07:02:44 +0530

    c class 3

---
+++ basic/temp.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int C,F;
+	printf("enter a temperature in Centigrade\n");
+	scanf("%d",&C);
+	F=(C*9/5)+32;
+	printf("Centigrade=%d\n",C);
+	printf("Farhenheit=%d\n",F);
+}
