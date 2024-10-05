b69fc6cb5116b4f4240e3b376443bd589c46d352
Sandeepandeveloper <sandeepan.n.behera@gmail.com>
2024-10-05 22:45:04 +0530

    c class4

---
+++ basic/circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	float r,P,A;
+	printf("enter a radius \n");
+	scanf("%f",&r);
+	P=2*3.14*r;
+	A=3.14*r*r;
+	printf("Perimeter of the Circle=%f\n",P);
+	printf("Area of the Circle=%f\n",A);
+}		
